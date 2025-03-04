fc8803c2a63133077aeaada0f2003ccb89d11e13
gmz007 <gao_mengze@outlook.com>
2025-03-04 12:58:38 +0800

    Added country images for language and fluent dialog

--- DemoApp/Constants.cs
+++ DemoApp/Constants.cs
@@ -1,21 +1,37 @@
-﻿namespace DemoApp
+﻿using DemoApp.Models;
+
+namespace DemoApp
 {
     public static class Constants
     {
-        public static readonly Dictionary<string, string> Languages = new()
-        {
-            { "简体中文", "zh-hans" },
-            { "繁體中文", "zh-hant" },
-            { "English", "en" },
-            { "Français", "fr" },
-            { "Español", "es" },
-        };
+
+        public static readonly LanguageInfo[] Languages =
+        [
+            new LanguageInfo("English", "en", new Uri(ResourceUri.UnitedStatesIcon)),
+            new LanguageInfo("Español", "es", new Uri(ResourceUri.SpainIcon)),
+            new LanguageInfo("Français", "fr", new Uri(ResourceUri.FranceIcon)),
+            new LanguageInfo("简体中文", "zh-hans", new Uri(ResourceUri.ChinaIcon)),
+            new LanguageInfo("繁體中文", "zh-hant", new Uri(ResourceUri.HongKongIcon)),
+        ];
 
         public static class Settings
         {
             public const string SettingsDirectory = "settings";
 
             public const string UserSettingsFile = "user_settings.json";
+        }
+
+        public static class ResourceUri
+        {
+            public const string ChinaIcon = "pack://application:,,,/assets/icons/china.png";
+
+            public const string FranceIcon = "pack://application:,,,/assets/icons/france.png";
+
+            public const string HongKongIcon = "pack://application:,,,/assets/icons/hong-kong.png";
+
+            public const string SpainIcon = "pack://application:,,,/assets/icons/spain.png";
+
+            public const string UnitedStatesIcon = "pack://application:,,,/assets/icons/united-states.png";
         }
     }
 }
---
+++ DemoApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,48 @@
+﻿using DemoApp.Helpers;
+using DemoApp.Models;
+using DemoApp.Mvvm.ViewModel;
+using DemoApp.Settings;
+using System.Collections.ObjectModel;
+using System.Globalization;
+
+namespace DemoApp.ViewModels
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        private readonly IUserSettings _userSettings;
+
+        public MainWindowViewModel(IUserSettings userSettings)
+        {
+            _userSettings = userSettings;
+
+            LanguageCollection = new ObservableCollection<LanguageInfo>(Constants.Languages);
+            SelectedLanguage =
+                Constants.Languages.FirstOrDefault(lang => lang.Code.Equals(userSettings.Language)) ?? Constants.Languages[0];
+
+            LocalizationSource.Instance.CurrentCulture = new CultureInfo(userSettings.Language);
+        }
+
+        public ObservableCollection<LanguageInfo> LanguageCollection { get; }
+
+        private LanguageInfo? _selectedLanguage;
+
+        public LanguageInfo SelectedLanguage
+        {
+            get => _selectedLanguage ?? Constants.Languages[0];
+            set
+            {
+                if (_selectedLanguage == value)
+                    return;
+
+                var index = Array.FindIndex(Constants.Languages, lang => lang.FullName.Equals(value.FullName));
+
+                if (index == -1) // TODO: Warning message?
+                    return;
+
+                LocalizationSource.Instance.CurrentCulture = new CultureInfo(Constants.Languages[index].Code);
+                _userSettings.Language = Constants.Languages[index].Code;
+                _selectedLanguage = value;
+            }
+        }
+    }
+}
